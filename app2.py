507db2e13697bd558b38010bf4bea4e0e749bc29
son-jy <sjo980706@gmail.com>
2024-09-12 15:28:10 +0900

    Add files via upload

---
+++ app2.py
@@ -0,0 +1,43 @@
+import plotly.express as px
+import plotly
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+# 한글 안깨지게 하는 코드 
+from matplotlib import font_manager, rc
+font = font_manager.FontProperties(fname="c:/Windows/Fonts/malgun.ttf").get_name()
+rc('font', family=font)
+
+#plt.figure(figsize=(12,8))
+
+money = pd.read_csv("c:\\data\\money_data7.csv")
+
+money = money[:] [money['A_YEAR']==2022]
+
+fig, ax = plt.subplots(2,2, figsize=(12,8))
+
+plt.subplot(221)
+plt.plot(  list( money['A_MONTH'] ), list( money['A_RATE'] ), color='red' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('미국금리')
+
+
+plt.subplot(222)
+plt.plot(  list( money['A_MONTH'] ), list( money['K_RATE'] ), color='blue' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('한국금리')
+
+plt.subplot(223)
+plt.plot(  list( money['A_MONTH'] ), list( money['KOSPI'] ), color='green' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('코스피 지수')
+
+plt.subplot(224)
+plt.plot(  list( money['A_MONTH'] ), list( money['HOUSE_PRICE'] ), color='yellow' , marker='o'     ) 
+plt.xticks(tuple(money['A_MONTH']) )
+plt.title('집값')
+
+st.pyplot(fig)
